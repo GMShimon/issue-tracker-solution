35a67f22f221a652fee1f2c129ae7d9c3700fea3
GMShimon <simon.rosedale99@gmail.com>
2021-09-10 23:08:28 +0600

    debugged

---
+++ main.js
@@ -0,0 +1,76 @@
+document.getElementById('issueInputForm').addEventListener('submit', submitIssue);
+
+function submitIssue(e) {
+    const getInputValue = id => document.getElementById(id).value;
+    const description = getInputValue('issueDescription');
+    const severity = getInputValue('issueSeverity');
+    const assignedTo = getInputValue('issueAssignedTo');
+    const id = Math.floor(Math.random() * 100000000) + '';
+    const status = 'Open';
+
+    const issue = {id, description, severity, assignedTo, status};
+    let issues = [];
+    if (localStorage.getItem('issues')) {
+        issues = JSON.parse(localStorage.getItem('issues'));
+    }
+    issues.push(issue);
+    localStorage.setItem('issues', JSON.stringify(issues));
+
+    document.getElementById('issueInputForm').reset();
+    fetchIssues();
+    e.preventDefault();
+}
+
+const setStatusClosed = id => {
+    const issues = JSON.parse(localStorage.getItem('issues'));
+    const status = (issue) => {
+        for (let i = 0; i < issue.length; i++) {
+            let number = parseInt(issue[i].id);
+            // console.log(id);
+            if (id === number) {
+                issue[i].status = 'Closed';
+                // console.log('hello');
+            }
+        }
+    };
+    status(issues);
+    localStorage.setItem('issues', JSON.stringify(issues));
+    fetchIssues();
+}
+
+const deleteIssue = id => {
+    const issues = JSON.parse(localStorage.getItem('issues'));
+    const result = [];
+    const deleteIssues = (issue) => {
+        for (let i = 0; i < issue.length; i++) {
+            // console.log(issue.length);
+            let number = parseInt(issue[i].id);
+            if (number !== id) {
+                result.push(issue[i]);
+            }
+        }
+    }
+    deleteIssues(issues);
+    localStorage.setItem('issues', JSON.stringify(result));
+    fetchIssues();
+}
+
+const fetchIssues = () => {
+    const issues = JSON.parse(localStorage.getItem('issues'));
+    const issuesList = document.getElementById('issuesList');
+    issuesList.innerHTML = '';
+
+    for (var i = 0; i < issues.length; i++) {
+        const {id, description, severity, assignedTo, status} = issues[i];
+
+        issuesList.innerHTML += `<div class="well">
+                              <h6>Issue ID: ${id} </h6>
+                              <p><span class="label label-info"> ${status} </span></p>
+                              <h3> ${description} </h3>
+                              <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
+                              <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
+                              <a href="#" onclick="setStatusClosed(${id})" class="btn btn-warning">Close</a>
+                              <a href="#" onclick="deleteIssue(${id})" class="btn btn-danger">Delete</a>
+                              </div>`;
+    }
+}
